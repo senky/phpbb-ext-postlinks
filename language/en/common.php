a2c749122f8a64696e7efbc481df8b18e3ec506f
Jakub Senko <jakubsenko@gmail.com>
2019-01-08 13:51:12 +0100

    Automatically copy to clipboard

--- language/en/common.php
+++ language/en/common.php
@@ -25,7 +25,7 @@ $lang = array_merge($lang, array(
 	'PL_HIDE'				=> 'Hide post links',
 	'PL_SHOW'				=> 'Show post links',
 
-	'CLICK_TO_COPY_LINK'	=> 'Click to select post’s link in plain format',
-	'CLICK_TO_COPY_BBCODE'	=> 'Click to select post’s link in BBcode format',
-	'CLICK_TO_COPY_HTML'	=> 'Click to select post’s link in HTML format',
+	'CLICK_TO_COPY_LINK'	=> 'Click to copy post’s link in plain format',
+	'CLICK_TO_COPY_BBCODE'	=> 'Click to copy post’s link in BBcode format',
+	'CLICK_TO_COPY_HTML'	=> 'Click to copy post’s link in HTML format',
 ));
